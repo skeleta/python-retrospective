2eaaab110bb900b55a14e487f82c0d6c042062a6
Иван Капукаранов <ivankapukaranov@gmail.com>
2013-04-27 17:56:28 +0300

    Remove repeats in task3

--- task3/solution.py
+++ task3/solution.py
@@ -1,6 +1,4 @@
 class Person:
-    family = {}
-
     def __init__(self, name, birth_year,
                  gender, father=None, mother=None):
         self.name = name
@@ -8,7 +6,6 @@ class Person:
         self.gender = gender
         self.father = father
         self.mother = mother
-        Person.family.update({self: [father, mother, self.gender]})
         self.kids = []
 
         if self.father:
@@ -28,40 +25,15 @@ class Person:
             return self.kids
 
     def get_sisters(self):
-        sisters = []
-        for key, value in Person.family.items():
-            if value[2] == 'F':
-                if key is not self:
-                    if self.father is None:
-                        if self.mother is value[1] and value[1] is not None:
-                            sisters.append(key)
-                    else:
-                        if self.father is value[0]:
-                            sisters.append(key)
-                        if self.mother is value[1] and value[1] is not None:
-                            if key not in sisters:
-                                sisters.append(key)
-        return sisters
+        return self._get_siblings("F")
 
     def get_brothers(self):
-        brothers = []
-        for key, value in Person.family.items():
-            if value[2] == 'M':
-                if key is not self:
-                    if self.father is None:
-                        if self.mother is value[1] and value[1] is not None:
-                            brothers.append(key)
-                    else:
-                        if self.father is value[0]:
-                            brothers.append(key)
-                        if self.mother is value[1] and value[1] is not None:
-                            if key not in brothers:
-                                brothers.append(key)
-        return brothers
+        return self._get_siblings("M")
 
-    def is_direct_successor(self, other):
-        for key, value in Person.family.items():
-            if (value[0] or value[1]) is self:
-                if key is other:
-                    return True
-        return False
+    def is_direct_successor(self, other_person):
+        return other_person in self.children()
+
+    def _get_siblings(self, gender):
+        all_siblings = list(self.mother.children() + self.father.children())
+        return list(set(sibling for sibling in all_siblings
+                    if sibling.gender == gender and sibling is not self))
